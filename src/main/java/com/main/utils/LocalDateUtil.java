0e07185f15d7daae1811f8226c8afd871764231d
wyf <weiyf@gegejia.com>
2019-06-18 16:05:45 +0800

    增加工具类

--- src/main/java/com/main/utils/LocalDateUtil.java
+++ src/main/java/com/main/utils/LocalDateUtil.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class LocalDateUtil {
 
-    public static void main(String[] args) {
+    public static void man(String[] args) {
 
         Date date = strToDate("2018-08-21 15:16:00", DatePattern.LONG);
 
