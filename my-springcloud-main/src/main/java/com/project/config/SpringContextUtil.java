33503294c234cf20dbed1a532b08e82d9f1366d7
weiyf <wyf3634@163.com>
2020-02-22 18:03:12 +0800

    增加contextutil

---
+++ my-springcloud-main/src/main/java/com/project/config/SpringContextUtil.java
@@ -0,0 +1,31 @@
+package com.project.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * SpringContextUtil
+ * 可以配合缓存注解使用
+ *
+ *@author: Weiyf
+ *@Date: 2020/2/22 17:57
+ */
+@Component
+public class SpringContextUtil implements ApplicationContextAware {
+    private static ApplicationContext appCtx;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        appCtx = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return appCtx;
+    }
+
+    public static Object getBean(Class clazz) {
+        return appCtx.getBean(clazz);
+    }
+}
